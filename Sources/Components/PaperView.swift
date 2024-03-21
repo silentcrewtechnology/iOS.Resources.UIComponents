b972dc3e724661365ecf1cac83269e62c7a0c141
Ilnur Mugaev <ilnur.mugaev@akbarsdigital.ru>
2024-03-21 14:28:00 +0300

    PCABO3-10019: Добавил public init в ViewProperties в PaperView.

--- Sources/Components/PaperView.swift
+++ Sources/Components/PaperView.swift
@@ -13,17 +13,43 @@ public final class PaperView: UIView {
     // MARK: - ViewProperties
     
     public struct ViewProperties {
-        var cornerRadius: CGFloat = 0
-        var maskedCorners: CACornerMask = []
-        var masksToBounds: Bool = true
-        var backgroundColor: UIColor = .clear
-        var shadow: Shadow = .init()
+        var cornerRadius: CGFloat
+        var maskedCorners: CACornerMask
+        var masksToBounds: Bool
+        var backgroundColor: UIColor
+        var shadow: Shadow
+        
+        public init(
+            cornerRadius: CGFloat = 0,
+            maskedCorners: CACornerMask = [],
+            masksToBounds: Bool = true,
+            backgroundColor: UIColor = .clear,
+            shadow: Shadow = .init()
+        ) {
+            self.cornerRadius = cornerRadius
+            self.maskedCorners = maskedCorners
+            self.masksToBounds = masksToBounds
+            self.backgroundColor = backgroundColor
+            self.shadow = shadow
+        }
         
         public struct Shadow {
-            var color: UIColor = .clear
-            var offset: CGSize = .zero
-            var opacity: Float = 0
-            var radius: CGFloat = 0
+            var color: UIColor
+            var offset: CGSize
+            var opacity: Float
+            var radius: CGFloat
+            
+            public init(
+                color: UIColor = .clear,
+                offset: CGSize = .zero,
+                opacity: Float = 0,
+                radius: CGFloat = 0
+            ) {
+                self.color = color
+                self.offset = offset
+                self.opacity = opacity
+                self.radius = radius
+            }
         }
     }
     
