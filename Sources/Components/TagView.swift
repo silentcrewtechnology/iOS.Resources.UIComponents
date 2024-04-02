080062fa56f8d114769879826e2ddc182db266dc
Eduard Sergeev <sergeev.ed@me.com>
2024-04-02 15:29:01 +0700

    PCABO3-10013 + добавление кликабельных кнопок к Tag

--- Sources/Components/TagView.swift
+++ Sources/Components/TagView.swift
@@ -3,26 +3,44 @@ import SnapKit
 
 public class TagView: UIView {
     
+    public typealias TapView = TapInsetView<UIImageView>
+    
     public struct ViewProperties {
+        public var height: CGFloat
         public var backgroundColor: UIColor
-        public var text: NSMutableAttributedString
         public var cornerRadius: CGFloat
+        public var leftProperties: TapView.ViewProperties?
+        public var text: NSMutableAttributedString
+        public var rightProperties: TapView.ViewProperties?
         public var insets: UIEdgeInsets
         
         public init(
+            height: CGFloat = .zero,
             backgroundColor: UIColor = .clear,
-            text: NSMutableAttributedString = .init(string: ""),
             cornerRadius: CGFloat = .zero,
+            leftProperties: TapView.ViewProperties? = nil,
+            text: NSMutableAttributedString = .init(string: ""),
+            rightProperties: TapView.ViewProperties? = nil,
             insets: UIEdgeInsets = .zero
         ) {
+            self.height = height
             self.backgroundColor = backgroundColor
-            self.text = text
             self.cornerRadius = cornerRadius
+            self.leftProperties = leftProperties
+            self.text = text
+            self.rightProperties = rightProperties
             self.insets = insets
         }
     }
     
     private var viewProperties: ViewProperties = .init()
+    
+    private lazy var hStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .center
+        return stack
+    }()
     
     private let textLabel: UILabel = {
         let label = UILabel()
@@ -37,24 +55,44 @@ public class TagView: UIView {
     required init?(coder: NSCoder) { fatalError() }
     
     private func setupView() {
-        addSubview(textLabel)
-        textLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview() // будет обновлено
-        }
+        snp.makeConstraints { $0.height.equalTo(0) } // будет обновлено
+        addSubview(hStack)
+        hStack.snp.makeConstraints { $0.edges.equalToSuperview() } // будет обновлено
     }
     
     public func update(with viewProperties: ViewProperties) {
         backgroundColor = viewProperties.backgroundColor
-        textLabel.attributedText = viewProperties.text
         layer.cornerRadius = viewProperties.cornerRadius
-        updateInsets(with: viewProperties)
+        textLabel.attributedText = viewProperties.text
+        updateHeight(height: viewProperties.height)
+        updateStack(with: viewProperties)
         self.viewProperties = viewProperties
     }
     
-    private func updateInsets(with viewProperties: ViewProperties) {
-        guard self.viewProperties.insets != viewProperties.insets else { return }
-        textLabel.snp.updateConstraints {
+    private func updateHeight(height: CGFloat) {
+        guard self.viewProperties.height != height else { return }
+        snp.updateConstraints { $0.height.equalTo(height) }
+    }
+    
+    private func updateStack(with viewProperties: ViewProperties) {
+        hStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        if let leftProperties = viewProperties.leftProperties {
+            hStack.addArrangedSubview(tappableView(with: leftProperties))
+        }
+        hStack.addArrangedSubview(textLabel)
+        if let rightProperties = viewProperties.rightProperties {
+            hStack.addArrangedSubview(tappableView(with: rightProperties))
+        }
+        hStack.snp.updateConstraints {
             $0.edges.equalToSuperview().inset(viewProperties.insets)
         }
+    }
+    
+    private func tappableView(
+        with viewProperties: TapView.ViewProperties
+    ) -> TapView {
+        let view = TapView()
+        view.update(with: viewProperties)
+        return view
     }
 }
