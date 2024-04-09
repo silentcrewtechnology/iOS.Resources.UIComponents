eac82321b1c5c7e34d17ee9231e2b833d6136ac7
Eduard Sergeev <sergeev.ed@me.com>
2024-04-09 16:22:13 +0700

    PCABO3-10014 + компонент TabItem

---
+++ Sources/Components/TabItemView.swift
@@ -0,0 +1,71 @@
+import UIKit
+import SnapKit
+
+public class TabItemView: UIView {
+    
+    public struct ViewProperties {
+        public var text: NSMutableAttributedString
+        public var rightView: UIView?
+        public var onTap: () -> Void
+        
+        public init(
+            text: NSMutableAttributedString = .init(string: ""),
+            rightView: UIView? = nil,
+            onTap: @escaping () -> Void = { }
+        ) {
+            self.text = text
+            self.rightView = rightView
+            self.onTap = onTap
+        }
+    }
+    
+    private var viewProperties: ViewProperties = .init()
+    
+    private let hStack: UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 8
+        stack.alignment = .center
+        return stack
+    }()
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError() }
+    
+    private func setupView() {
+        addSubview(hStack)
+        hStack.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(8)
+            $0.center.equalToSuperview()
+        }
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+    }
+    
+    public func update(with viewProperties: ViewProperties) {
+        textLabel.attributedText = viewProperties.text
+        updateStack(with: viewProperties)
+        self.viewProperties = viewProperties
+    }
+    
+    private func updateStack(with viewProperties: ViewProperties) {
+        hStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        hStack.addArrangedSubview(textLabel)
+        
+        if let rightView = viewProperties.rightView {
+            hStack.addArrangedSubview(rightView)
+        }
+    }
+    
+    @objc private func tapped() {
+        viewProperties.onTap()
+    }
+}
