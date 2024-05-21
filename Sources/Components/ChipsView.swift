40782191e516cc9466961b9df2aa7dfe23e4ba14
Сергеев Эдуард Олегович <sergeeveo@akbarsdigital.ru>
2024-05-21 20:50:34 +0700

    PCABO3-10669 ~ указание height и cornerRadius во ViewProperties

--- Sources/Components/ChipsView.swift
+++ Sources/Components/ChipsView.swift
@@ -4,7 +4,9 @@ import SnapKit
 public class ChipsView: PressableView {
     
     public struct ViewProperties {
+        public var height: CGFloat
         public var backgroundColor: UIColor
+        public var cornerRadius: CGFloat
         public var leftView: UIView?
         public var text: NSMutableAttributedString
         public var rightView: UIView?
@@ -13,7 +15,9 @@ public class ChipsView: PressableView {
         public var onPressChange: (State) -> Void
         
         public init(
+            height: CGFloat = 0,
             backgroundColor: UIColor = .clear,
+            cornerRadius: CGFloat = 0,
             leftView: UIView? = nil,
             text: NSMutableAttributedString = .init(string: ""),
             rightView: UIView? = nil,
@@ -21,7 +25,9 @@ public class ChipsView: PressableView {
             isUserInteractionEnabled: Bool = true,
             onPressChange: @escaping (State) -> Void = { _ in }
         ) {
+            self.height = height
             self.backgroundColor = backgroundColor
+            self.cornerRadius = cornerRadius
             self.leftView = leftView
             self.text = text
             self.rightView = rightView
@@ -58,6 +64,7 @@ public class ChipsView: PressableView {
         addSubview(hStack)
         hStack.snp.makeConstraints {
             $0.edges.equalToSuperview() // будет обновлено
+            $0.height.equalTo(0) // будет обновлено
         }
         textLabel.snp.makeConstraints {
             $0.width.lessThanOrEqualTo(200)
@@ -66,10 +73,12 @@ public class ChipsView: PressableView {
     
     public func update(with viewProperties: ViewProperties) {
         backgroundColor = viewProperties.backgroundColor
+        layer.cornerRadius = viewProperties.cornerRadius
         textLabel.attributedText = viewProperties.text
         isUserInteractionEnabled = viewProperties.isUserInteractionEnabled
         updateStack(with: viewProperties)
         updateInsets(with: viewProperties)
+        updateHeight(with: viewProperties)
         self.viewProperties = viewProperties
     }
     
@@ -94,9 +103,11 @@ public class ChipsView: PressableView {
         }
     }
     
-    public override func layoutSubviews() {
-        super.layoutSubviews()
-        layer.cornerRadius = bounds.height / 2
+    private func updateHeight(with viewProperties: ViewProperties) {
+        guard self.viewProperties.height != viewProperties.height else { return }
+        hStack.snp.updateConstraints {
+            $0.height.equalTo(viewProperties.height)
+        }
     }
     
     public override func handlePress(state: State) {
