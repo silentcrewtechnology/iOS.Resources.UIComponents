e0998ee888462a79925897073c22250709b22ed6
Сергеев Эдуард Олегович <sergeeveo@akbarsdigital.ru>
2024-05-23 19:11:18 +0700

    PCABO3-10693 ~ актуализация компонента Radio

--- Sources/Components/RadioView.swift
+++ Sources/Components/RadioView.swift
@@ -1,127 +1,94 @@
 import UIKit
 import SnapKit
 
-public final class RadioView: UIView {
+public final class RadioView: PressableView {
     
     public struct ViewProperties {
-        public var background: Background
-        public var onIndicator: Indicator
-        public var offIndicator: Indicator
+        public var backgroundColor: UIColor
+        public var size: CGSize
+        public var cornerRadius: CGFloat
+        public var borderColor: UIColor
+        public var borderWidth: CGFloat
+        public var checkIcon: UIImage?
+        public var isUserInteractionEnabled: Bool
+        public var onPressChange: (State) -> Void
         
         public init(
-            background: Background = .init(),
-            onIndicator: Indicator = .init(),
-            offIndicator: Indicator = .init()
+            backgroundColor: UIColor = .clear,
+            size: CGSize = .zero,
+            cornerRadius: CGFloat = 0,
+            borderColor: UIColor = .clear,
+            borderWidth: CGFloat = 0,
+            checkIcon: UIImage? = nil,
+            isUserInteractionEnabled: Bool = true,
+            onPressChange: @escaping (State) -> Void = { _ in }
         ) {
-            self.background = background
-            self.onIndicator = onIndicator
-            self.offIndicator = offIndicator
-        }
-        
-        public struct Background {
-            public var color: UIColor
-            public var size: CGFloat
-            
-            public init(
-                color: UIColor = .clear,
-                size: CGFloat = .zero
-            ) {
-                self.color = color
-                self.size = size
-            }
-        }
-        
-        public struct Indicator {
-            public var color: UIColor
-            public var size: CGFloat
-            
-            public init(
-                color: UIColor = .clear,
-                size: CGFloat = .zero
-            ) {
-                self.color = color
-                self.size = size
-            }
+            self.backgroundColor = backgroundColor
+            self.size = size
+            self.cornerRadius = cornerRadius
+            self.borderColor = borderColor
+            self.borderWidth = borderWidth
+            self.checkIcon = checkIcon
+            self.isUserInteractionEnabled = isUserInteractionEnabled
+            self.onPressChange = onPressChange
         }
     }
     
     private var viewProperties: ViewProperties = .init()
     
-    // MARK: - UI
-    
-    private let onIndicatorView: UIView = {
-        let view = UIView()
+    private let indicatorView: UIImageView = {
+        let view = UIImageView()
         return view
     }()
-    
-    private let offIndicatorView: UIView = {
-        let view = UIView()
-        return view
-    }()
-    
-    // MARK: - Init
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
     
-    @available(*, unavailable) required init?(coder: NSCoder) { fatalError() }
-    
-    // MARK: - Public Methods
-    
-    public func update(with viewProperties: ViewProperties) {
-        setupBackground(background: viewProperties.background)
-        setupOnIndicator(indicator: viewProperties.onIndicator)
-        setupOffIndicator(indicator: viewProperties.offIndicator)
-        self.viewProperties = viewProperties
-    }
-    
-    // MARK: - Private Methods
+    required init?(coder: NSCoder) { fatalError() }
     
     private func setupView() {
+        layer.masksToBounds = true
         snp.makeConstraints {
             $0.size.equalTo(0) // будет обновляться
         }
-        addSubview(onIndicatorView)
-        onIndicatorView.snp.makeConstraints {
+        addSubview(indicatorView)
+        indicatorView.snp.makeConstraints {
             $0.center.equalToSuperview()
-            $0.size.equalTo(0) // будет обновляться
-        }
-        addSubview(offIndicatorView)
-        offIndicatorView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.size.equalTo(0) // будет обновляться
         }
     }
     
-    private func setupBackground(background: ViewProperties.Background) {
-        backgroundColor = background.color
-        if self.viewProperties.background.size != background.size {
-            layer.cornerRadius = background.size / 2
+    public func update(with viewProperties: ViewProperties) {
+        isUserInteractionEnabled = viewProperties.isUserInteractionEnabled
+        setupBackground(with: viewProperties)
+        setupBorder(with: viewProperties)
+        setupIndicator(with: viewProperties)
+        self.viewProperties = viewProperties
+    }
+    
+    private func setupBackground(with viewProperties: ViewProperties) {
+        backgroundColor = viewProperties.backgroundColor
+        if self.viewProperties.size != viewProperties.size {
             snp.updateConstraints {
-                $0.size.equalTo(background.size)
+                $0.size.equalTo(viewProperties.size)
             }
         }
+        layer.cornerRadius = viewProperties.cornerRadius
     }
     
-    private func setupOnIndicator(indicator: ViewProperties.Indicator) {
-        onIndicatorView.backgroundColor = indicator.color
-        if self.viewProperties.onIndicator.size != indicator.size {
-            onIndicatorView.layer.cornerRadius = indicator.size / 2
-            onIndicatorView.snp.updateConstraints {
-                $0.size.equalTo(indicator.size)
-            }
-        }
+    private func setupBorder(with viewProperties: ViewProperties) {
+        layer.borderColor = viewProperties.borderColor.cgColor
+        layer.borderWidth = viewProperties.borderWidth
     }
     
-    private func setupOffIndicator(indicator: ViewProperties.Indicator) {
-        offIndicatorView.backgroundColor = indicator.color
-        if self.viewProperties.offIndicator.size != indicator.size {
-            offIndicatorView.layer.cornerRadius = indicator.size / 2
-            offIndicatorView.snp.updateConstraints {
-                $0.size.equalTo(indicator.size)
-            }
+    private func setupIndicator(with viewProperties: ViewProperties) {
+        indicatorView.image = viewProperties.checkIcon
+    }
+    
+    public override func handlePress(state: State) {
+        UIView.animate(withDuration: 0.2) {
+            self.viewProperties.onPressChange(state)
         }
     }
 }
