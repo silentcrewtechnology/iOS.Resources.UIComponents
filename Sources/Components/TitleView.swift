9b1eb2a392712e3fe601598eb4066125b2fabd1b
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-07-23 14:46:13 +0300

    PCABO3-11165: Обновил TitleView

--- Sources/Components/TitleView.swift
+++ Sources/Components/TitleView.swift
@@ -14,126 +14,67 @@ public class TitleView: UIView {
     
     public struct ViewProperties {
         public var title: NSMutableAttributedString
-        public var description: NSMutableAttributedString?
-        public var iconButtonViewProperties: ButtonIcon.ViewProperties?
         public var backgroundColor: UIColor
         public var insets: UIEdgeInsets
-        public var space: CGFloat
         
         public init(
             title: NSMutableAttributedString = .init(string: ""),
-            description: NSMutableAttributedString? = nil,
-            iconButtonViewProperties: ButtonIcon.ViewProperties? = nil,
             backgroundColor: UIColor = .clear,
-            insets: UIEdgeInsets = .zero,
-            space: CGFloat = .zero
+            insets: UIEdgeInsets = .zero
         ) {
             self.title = title
-            self.description = description
-            self.iconButtonViewProperties = iconButtonViewProperties
             self.backgroundColor = backgroundColor
             self.insets = insets
-            self.space = space
         }
     }
     
+    // MARK: - Private properties
+    
     private var viewProperties: ViewProperties = .init()
-    
-    // MARK: - UI
-    
+
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.numberOfLines = .zero
+        
         return label
     }()
     
-    private lazy var descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private lazy var iconButton: ButtonIcon = {
-        let button = ButtonIcon()
-        return button
-    }()
-    
-    private lazy var hStack: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [
-            titleLabel,
-            iconButton
-        ])
-        stackView.axis = .horizontal
-        stackView.alignment = .top
-        stackView.spacing = 4
-        return stackView
-    }()
-    
-    private lazy var vStack: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [
-            hStack,
-            descriptionLabel
-        ])
-        stackView.axis = .vertical
-        return stackView
-    }()
-    
-    // MARK: - Init
+    // MARK: - Life cycle
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
+        
         setupView()
     }
     
     required init?(coder: NSCoder) { fatalError() }
     
-    private func setupView() {
-        addSubview(vStack)
-        vStack.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-    }
-    
     // MARK: - Public method
     
     public func update(with viewProperties: ViewProperties) {
-        updateLabel(titleLabel, text: viewProperties.title)
-        updateLabel(descriptionLabel, text: viewProperties.description)
-        updateIconButton(viewProperties.iconButtonViewProperties)
-        updateAlignment()
-        updateInsets(viewProperties.insets)
-        vStack.spacing = viewProperties.space
-        backgroundColor = viewProperties.backgroundColor
         self.viewProperties = viewProperties
+        
+        backgroundColor = viewProperties.backgroundColor
+        updateLabel(text: viewProperties.title)
+        updateInsets(viewProperties.insets)
     }
     
     // MARK: - Private methods
     
-    private func updateLabel(_ label: UILabel, text: NSAttributedString?) {
-        if let text {
-            label.attributedText = text
-            label.isHidden = false
-        } else {
-            label.isHidden = true
+    private func setupView() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
     
-    private func updateIconButton(_ iconButtonViewProperties: ButtonIcon.ViewProperties?) {
-        if let iconButtonViewProperties {
-            iconButton.update(with: iconButtonViewProperties)
-            iconButton.isHidden = false
-        } else {
-            iconButton.isHidden = true
-        }
-    }
-    
-    private func updateAlignment() {
-        guard !iconButton.isHidden else { return }
-        hStack.alignment = descriptionLabel.isHidden ? .center : .top
+    private func updateLabel(text: NSAttributedString?) {
+        titleLabel.isHidden = text != nil ? false : true
+        titleLabel.attributedText = text
     }
     
     private func updateInsets(_ insets: UIEdgeInsets) {
-        vStack.snp.updateConstraints {
+        titleLabel.snp.updateConstraints {
             $0.edges.equalToSuperview().inset(insets)
         }
     }
