ab1ceb46eeae3b9d2b1c6e3da50e1a643798412f
Омельченко Юлия Константиновна <omelchenkoyuk@akbarsdigital.ru>
2024-07-26 11:42:36 +0300

    PCABO3-11166: Изменила ImageView по новому дизайну

--- Sources/Components/ImageView.swift
+++ Sources/Components/ImageView.swift
@@ -1,7 +1,9 @@
 import UIKit
 import SnapKit
 
-public class ImageView: UIView {
+public final class ImageView: UIView {
+    
+    // MARK: - ViewProperties
     
     public struct ViewProperties {
         public var size: CGSize
@@ -9,23 +11,50 @@ public class ImageView: UIView {
         public var cornerRadius: CGFloat
         public var text: NSMutableAttributedString?
         public var image: UIImage?
+        public var margins: Margins
         
         public init(
             size: CGSize = .zero,
             backgroundColor: UIColor = .clear,
             cornerRadius: CGFloat = 0,
             text: NSMutableAttributedString? = nil,
-            image: UIImage? = nil
+            image: UIImage? = nil,
+            margins: Margins = .init()
         ) {
             self.size = size
             self.backgroundColor = backgroundColor
             self.cornerRadius = cornerRadius
             self.text = text
             self.image = image
+            self.margins = margins
+        }
+        
+        public struct Margins {
+            public var imageTop: CGFloat
+            public var imageBottom: CGFloat
+            public var imageLeading: CGFloat
+            public var imageTrailing: CGFloat
+            public var size: CGSize
+            
+            public init(
+                imageTop: CGFloat = 0,
+                imageBottom: CGFloat = 0,
+                imageLeading: CGFloat = 0,
+                imageTrailing: CGFloat = 0,
+                size: CGSize = .zero
+            ) {
+                self.imageTop = imageTop
+                self.imageBottom = imageBottom
+                self.imageLeading = imageLeading
+                self.imageTrailing = imageTrailing
+                self.size = size
+            }
         }
     }
     
     private var viewProperties: ViewProperties = .init()
+    
+    // MARK: - UI
     
     private let imageView: UIImageView = {
         let view = UIImageView()
@@ -39,48 +68,37 @@ public class ImageView: UIView {
         return label
     }()
     
+    // MARK: - init
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
-    }
-    
-    private func setupView() {
-        addSubview(titleLabel)
-        titleLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.leading.greaterThanOrEqualToSuperview()
-            $0.trailing.lessThanOrEqualToSuperview()
-        }
-        
-        addSubview(imageView)
-        imageView.snp.makeConstraints() {
-            $0.edges.equalToSuperview()
-        }
-        
-        snp.makeConstraints {
-            $0.size.equalTo(0) // будет обновлено
-        }
     }
     
     required init?(coder: NSCoder) { fatalError() }
     
     public func update(with viewProperties: ViewProperties) {
-        updateSize(with: viewProperties)
-        updateBackground(with: viewProperties)
-        updateText(with: viewProperties)
-        updateImage(with: viewProperties)
-        self.viewProperties = viewProperties
+        DispatchQueue.main.async {
+            self.viewProperties = viewProperties
+            self.updateBackground(with: viewProperties)
+            self.setCornerRadius(with: viewProperties)
+            self.updateText(with: viewProperties)
+            self.updateImage(with: viewProperties)
+            self.updateConstraints(with: viewProperties)
+        }
     }
     
-    private func updateSize(with viewProperties: ViewProperties) {
-        guard self.viewProperties.size != viewProperties.size else { return }
-        snp.updateConstraints { $0.size.equalTo(viewProperties.size) }
-    }
+    // MARK: - private methods
     
     private func updateBackground(with viewProperties: ViewProperties) {
         backgroundColor = viewProperties.backgroundColor
-        layer.cornerRadius = viewProperties.cornerRadius
-        clipsToBounds = true
+    }
+    
+    private func setCornerRadius(with viewProperties: ViewProperties) {
+        cornerRadius(
+            radius: viewProperties.cornerRadius,
+            direction: .allCorners,
+            clipsToBounds: true
+        )
     }
     
     private func updateText(with viewProperties: ViewProperties) {
@@ -89,5 +107,36 @@ public class ImageView: UIView {
     
     private func updateImage(with viewProperties: ViewProperties) {
         imageView.image = viewProperties.image
+    }
+    
+    private func updateConstraints(with viewProperties: ViewProperties) {
+        removeConstraintsAndSubviews()
+        setupTitleLabel()
+        setupImageView()
+    }
+    
+    private func setupTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func setupImageView() {
+        addSubview(imageView)
+        imageView.snp.makeConstraints() {
+            $0.top.equalToSuperview().offset(viewProperties.margins.imageTop)
+            $0.leading.equalToSuperview().offset(viewProperties.margins.imageLeading)
+            $0.trailing.equalToSuperview().offset(-viewProperties.margins.imageTrailing)
+            $0.bottom.equalToSuperview().offset(-viewProperties.margins.imageBottom)
+            $0.size.equalTo(viewProperties.margins.size)
+        }
+    }
+    
+    private func removeConstraintsAndSubviews() {
+        self.subviews.forEach { subview in
+            subview.snp.removeConstraints()
+            subview.removeFromSuperview()
+        }
     }
 }
