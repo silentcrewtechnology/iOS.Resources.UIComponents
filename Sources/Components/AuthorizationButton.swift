1f9bb9d65313335292f540d0aa70ae1680d30579
Омельченко Юлия Константиновна <omelchenkoyuk@akbarsdigital.ru>
2024-08-05 15:41:13 +0300

    PCABO3-11207: Актуализировала AuthorizationButton

--- Sources/Components/AuthorizationButton.swift
+++ Sources/Components/AuthorizationButton.swift
@@ -1,75 +1,61 @@
-//
-//  AuthorizationButton.swift
-//
-//
-//  Created by Ilnur Mugaev on 20.03.2024.
-//
-
 import UIKit
 import SnapKit
 
-public class AuthorizationButton: UIButton {
+public final class AuthorizationButton: UIButton {
+    
+    // MARK: - ViewProperties
     
     public struct ViewProperties {
         public var image: UIImage
         public var title: NSMutableAttributedString
         public var backgroundColor: UIColor
-        public var highlightedColor: UIColor
         public var spacing: CGFloat
+        public var cornerRadius: CGFloat
+        public var height: CGFloat
         public var onTap: () -> Void
         
         public init(
             image: UIImage = .init(),
             title: NSMutableAttributedString = .init(string: ""),
             backgroundColor: UIColor = .clear,
-            highlightedColor: UIColor = .clear,
             spacing: CGFloat = .zero,
+            cornerRadius: CGFloat = 0,
+            height: CGFloat = 0,
             onTap: @escaping () -> Void = { }
         ) {
             self.image = image
             self.title = title
             self.backgroundColor = backgroundColor
-            self.highlightedColor = highlightedColor
             self.spacing = spacing
+            self.cornerRadius = cornerRadius
+            self.height = height
             self.onTap = onTap
-        }
-    }
-    
-    public override var isHighlighted: Bool {
-        didSet {
-            backgroundColor = isHighlighted
-            ? viewProperties.highlightedColor
-            : viewProperties.backgroundColor
         }
     }
     
     private var viewProperties: ViewProperties = .init()
     
+    // MARK: - init
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        setupView()
     }
     
     required init?(coder: NSCoder) { fatalError() }
     
-    private func setupView() {
-        layer.cornerRadius = 8
-        layer.masksToBounds = true
-        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
-        snp.makeConstraints {
-            $0.height.equalTo(56)
-        }
-    }
-    
     public func update(with viewProperties: ViewProperties) {
+        self.viewProperties = viewProperties
         updateImage(image: viewProperties.image)
         updateTitle(title: viewProperties.title)
         updateInsets(spacing: viewProperties.spacing)
+        updateCornerRadius(with: viewProperties)
+        updateHeight(with: viewProperties)
         backgroundColor = viewProperties.backgroundColor
-        self.viewProperties = viewProperties
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
+    // MARK: - private methods
+
     private func updateImage(image: UIImage) {
         setImage(image, for: .normal)
         setImage(image, for: .highlighted)
@@ -89,5 +75,19 @@ public class AuthorizationButton: UIButton {
     
     @objc private func buttonTapped() {
         viewProperties.onTap()
+    }
+    
+    private func updateCornerRadius(with viewProperties: ViewProperties) {
+        cornerRadius(
+            radius: viewProperties.cornerRadius,
+            direction: .allCorners,
+            clipsToBounds: true
+        )
+    }
+    
+    private func updateHeight(with viewProperties: ViewProperties) {
+        snp.makeConstraints {
+            $0.height.equalTo(viewProperties.height)
+        }
     }
 }
